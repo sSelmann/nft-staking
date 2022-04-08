3e4ca558bbaab138bf12a9ff33ca5c1b965ff4c9
MelandriST <yusuf_480@hotmail.com>
2022-04-08 08:22:54 +0300

    stake contract fix

--- contracts/StakerToken.sol
+++ contracts/StakerToken.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockERC20 is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Staker Token", "STKR") {
-        _mint(msg.sender, initialSupply);
+    constructor(uint256 initialSupply, address stake_Contract) ERC20("Staker Token", "STKR") {
+        _mint(stake_Contract, initialSupply);
     }
 }
